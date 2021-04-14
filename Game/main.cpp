beb4a84441fb604a87092a6e3678a5635b7e2688
Tom Nagelgast <tomnagelgast@gmail.com>
2021-04-14 23:35:38 +0200

    Added placeholder HUD

--- Game/main.cpp
+++ Game/main.cpp
@@ -3,6 +3,7 @@
 #include "../Opus/Game.h"
 #include "Player.h"
 #include "Wall.h"
+#include "../Opus/ShapeRenderer.h"
 
 const int kScreenWidth = 1024;
 const int kScreenHeight = 768;
@@ -17,10 +18,28 @@ int main()
 	{
 		const auto player = game->entity_controller_.AddEntity(Player());
 
-		auto main_camera = game->entity_controller_.AddEntity(Entity());
-		auto camera = main_camera->AddComponent(Camera());
+		Entity main_camera {};
+		auto camera = main_camera.AddComponent<Camera>();
 		camera->SetTarget(player);
 		game->renderer_->SetCamera(camera);
+		game->entity_controller_.AddEntity(std::move(main_camera));
+
+		const auto margin = 120;
+		
+		Entity health_globe {};
+		health_globe.AddComponent(ShapeRenderer(Shape::kCircle, 1,0,0,1, false));
+		health_globe.SetShape(Shape::kCircle);
+		health_globe.SetPosition(margin, kScreenHeight-margin);
+		health_globe.SetSize(100, 100);
+		game->entity_controller_.AddEntity(std::move(health_globe));
+
+		Entity mana_globe {};
+		mana_globe.AddComponent(ShapeRenderer(Shape::kCircle, 0, 0, 1, 1, false));
+		mana_globe.SetShape(Shape::kCircle);
+		mana_globe.SetPosition(kScreenWidth-margin, kScreenHeight - margin);
+		mana_globe.SetSize(100, 100);
+		game->entity_controller_.AddEntity(std::move(mana_globe));
+
 
 		game->entity_controller_.AddEntity(Wall());
 		auto wall = game->entity_controller_.AddEntity(Wall());
