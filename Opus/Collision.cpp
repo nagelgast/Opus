b0feabdb53e5dbdea86c9647cb8af898d020b6f3
Tom Nagelgast <tomnagelgast@gmail.com>
2021-04-14 22:41:13 +0200

    Cleaned up filters

--- Opus/Collision.cpp
+++
@@ -1,9 +0,0 @@
-#include "pch.h"
-#include "Collision.h"
-
-#include "Entity.h"
-#include "Vector2.h"
-
-Collision::Collision(Entity* const e, const Vector2* const v): hit(true), other(e), displacement(v)
-{
-}
--- Opus/Collision.h
+++ Opus/Collision.h
@@ -6,7 +6,7 @@ class Entity;
 struct __declspec(dllexport) Collision
 {
 	Collision() = default;
-	Collision(Entity*, const Vector2*);
+	Collision(Entity* const e, const Vector2* const v) : hit(true), other(e), displacement(v) {}
 	const bool hit = false;
 	Entity* const other = nullptr;
 	const Vector2* const displacement = nullptr;
