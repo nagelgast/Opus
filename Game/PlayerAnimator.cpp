55d13e8f6332a66a2f2e29748305f21c7a7cdaca
Tom Nagelgast <tomnagelgast@gmail.com>
2021-12-31 16:08:14 +0100

    Added sprites when moving upwards

--- Game/PlayerAnimator.cpp
+++ Game/PlayerAnimator.cpp
@@ -6,43 +6,45 @@
 
 void PlayerAnimator::Awake()
 {
-    // TODO Implement way to pre-load all the textures and sprites
-    const Sprite sprite{ "Sprites/characters/idle_whit_sword_front.png", {0, 0, 48, 32} };
-    const auto sr = entity_->AddComponent(SpriteRenderer());
-    sr.SetSprite(sprite);
+	state_ = "idle_whit_sword";
+    sr_ = &entity_->AddComponent(SpriteRenderer());
+	UpdateSprite();
 
-    anim_ = &entity_->AddComponent(Animation(8, 0.1f));
+	anim_ = &entity_->AddComponent(Animation(8, 0.1f));
 }
-
-void PlayerAnimator::Start()
-{
-	sr_ = entity_->GetComponent<SpriteRenderer>();
-}
-
 
 void PlayerAnimator::Update()
 {
+	// TODO Implement FSM instead of using bools
+
 	const auto new_pos = entity_->GetTransform().GetPosition();
 	const auto delta_pos = new_pos - old_pos_;
-	const auto moving_right = delta_pos.x > 0;
-	const auto moving = moving_right || delta_pos.x < 0;
 
-	if(moving && facing_right_ != moving_right)
+	const auto moving_right = delta_pos.x > 0;
+	const auto moving_hor = moving_right || delta_pos.x < 0;
+	if(moving_hor && facing_right_ != moving_right)
 	{
 		facing_right_ = moving_right;
-		sr_->SetMirrored(!moving_right);
+		UpdateSprite();
+	}
+
+	const auto moving_down = delta_pos.y > 0;
+	const auto moving_ver = moving_down or delta_pos.y < 0;
+	if(moving_ver && facing_down_ != moving_down)
+	{
+		facing_down_ = moving_down;
+		UpdateSprite();
 	}
 
 	old_pos_ = new_pos;
 }
-//
-// void PlayerAnimator::StartMoving()
-// {
-// 	anim_->Play();
-// 	auto x = entity_->GetTransform().GetPosition().x;
-// 	// Mirror if we're moving into our opposite direction
-// 	if (target_pos_.x < x != sr_->IsMirrored())
-// 	{
-// 		sr_->Mirror();
-// 	}
-// }
+
+void PlayerAnimator::UpdateSprite() const
+{
+	sr_->SetMirrored(facing_right_ != facing_down_);
+
+	// TODO Implement way to pre-load all the textures and sprites
+	const auto facing = facing_down_ ? "front" : "back";
+	const Sprite sprite{ "Sprites/characters/" + state_ + "_" + facing + ".png", {0, 0, 48, 32} };
+	sr_->SetSprite(sprite);
+}
--- Game/PlayerAnimator.h
+++ Game/PlayerAnimator.h
@@ -11,12 +11,16 @@ class PlayerAnimator :
 {
 public:
     void Awake() override;
-    void Start() override;
     void Update() override;
 private:
+    void UpdateSprite() const;
+
     SpriteRenderer* sr_ = nullptr;
     Animation* anim_ = nullptr;
 
     Vector2 old_pos_;
     bool facing_right_ = true;
+    bool facing_down_ = true;
+
+    std::string state_;
 };
