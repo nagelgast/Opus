c86a176de5ae175235995ed0b8095af8d40154a4
Tom Nagelgast <tomnagelgast@gmail.com>
2022-01-19 18:54:38 +0100

    Fixed offset of item info popup

--- Game/ItemInfoPopup.cpp
+++ Game/ItemInfoPopup.cpp
@@ -3,11 +3,11 @@
 
 #include "Item.h"
 #include "Rarity.h"
-#include "Stat.h"
 #include "../Opus/ShapeRenderer.h"
 #include "../Opus/TextRenderer.h"
 
-const float kMargin = 10;
+constexpr float kMargin = 10;
+constexpr float kInfoY = 30;
 
 void ItemInfoPopup::Awake()
 {
@@ -15,59 +15,55 @@ void ItemInfoPopup::Awake()
 	background.GetTransform().SetScale(100, 200);
 	background_renderer_ = &background.AddComponent(ShapeRenderer(Shape::kSquare, {0.1f, 0.1f, 0.1f, 0.5f}, false));
 
-
 	auto& title_background = CreateChild();
 	title_background.GetTransform().SetScale(100, 30);
 	title_background_renderer_ = &title_background.AddComponent(ShapeRenderer(Shape::kSquare, kUniqueItemColor, false));
 
-	auto& title = CreateChild();
-	title_renderer_ = &title.AddComponent(TextRenderer());
+	title_renderer_ = &CreateChild<TextRenderer>();
 
-	auto& info = CreateChild();
-	info.GetTransform().SetLocalPosition({0, 30});
-	info_renderer_ = &info.AddComponent(TextRenderer());
+	info_renderer_ = &CreateChild<TextRenderer>();
+	info_renderer_->GetTransform().SetLocalPosition({0, kInfoY});
 }
 
 void ItemInfoPopup::SetItem(const Item& item) const
 {
-	Color title_color {};
+	Color title_color{};
 	switch (item.GetRarity())
 	{
-		case Rarity::kNormal:
-			title_color = kNormalItemColor;
-			break;
-		case Rarity::kMagic:
-			title_color = kMagicItemColor;
-			break;
-		case Rarity::kRare:
-			title_color = kRareItemColor;
-			break;
-		case Rarity::kUnique:
-			title_color = kUniqueItemColor;
-			break;
-		case Rarity::kCurrency:
-			title_color = kCurrencyItemColor;
-			break;
+	case Rarity::kNormal:
+		title_color = kNormalItemColor;
+		break;
+	case Rarity::kMagic:
+		title_color = kMagicItemColor;
+		break;
+	case Rarity::kRare:
+		title_color = kRareItemColor;
+		break;
+	case Rarity::kUnique:
+		title_color = kUniqueItemColor;
+		break;
+	case Rarity::kCurrency:
+		title_color = kCurrencyItemColor;
+		break;
 	}
-
-	title_background_renderer_->SetColor(title_color);
-
-	title_renderer_->SetText(item.GetName());
-	const auto title_width = title_renderer_->GetWidth();
 
 	const auto info_text = item.GenerateText();
 	info_renderer_->SetText(info_text);
+	title_background_renderer_->SetColor(title_color);
+	title_renderer_->SetText(item.GetName());
 
+	const auto title_width = title_renderer_->GetWidth();
 	const auto info_width = info_renderer_->GetWidth();
-
 	const auto width = std::max(title_width, info_width);
-	
+
 	Resize(*title_background_renderer_, width);
 	Resize(*background_renderer_, width);
 }
 
-void ItemInfoPopup::Resize(ShapeRenderer& sr, const float width)
+void ItemInfoPopup::Resize(const ShapeRenderer& sr, const float width)
 {
 	auto& transform = sr.entity_->GetTransform();
-	transform.SetScale(width + kMargin, transform.GetScale().y);
+	const auto scale_y = transform.GetScale().y;
+	transform.SetScale(width + kMargin, scale_y);
+	transform.SetLocalPosition({width / 2, scale_y / 2});
 }
--- Game/ItemInfoPopup.h
+++ Game/ItemInfoPopup.h
@@ -7,12 +7,11 @@ class Item;
 class TextRenderer;
 class ShapeRenderer;
 
-const Color kNormalItemColor = {0.78f,0.78f,0.78f};			// 200 200 200
-const Color kMagicItemColor = { 0.53f,0.53f,1};				// 136 136 255
-const Color kRareItemColor = { 1,1,0.47f};					// 255 255 119
-const Color kUniqueItemColor = { 0.69f, 0.38f, 0.15f};		// 175  96  37
-const Color kCurrencyItemColor = { 0.67f, 0.62f, 0.51f};		// 170 158 130
-
+constexpr Color kNormalItemColor = {0.78f,0.78f,0.78f};			// 200 200 200
+constexpr Color kMagicItemColor = { 0.53f,0.53f,1};				// 136 136 255
+constexpr Color kRareItemColor = { 1,1,0.47f};					// 255 255 119
+constexpr Color kUniqueItemColor = { 0.69f, 0.38f, 0.15f};		// 175  96  37
+constexpr Color kCurrencyItemColor = { 0.67f, 0.62f, 0.51f};		// 170 158 130
 
 class ItemInfoPopup : public Component
 {
@@ -20,7 +19,7 @@ public:
 	void Awake() override;
 	void SetItem(const Item& item) const;
 private:
-	static void Resize(ShapeRenderer& sr, float width);
+	static void Resize(const ShapeRenderer& sr, float width);
 
 	ShapeRenderer* background_renderer_ = nullptr;
 	ShapeRenderer* title_background_renderer_ = nullptr;
